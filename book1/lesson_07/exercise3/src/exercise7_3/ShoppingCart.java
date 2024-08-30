671918dda8de95e2d198c18f8aba4afc81411aaa
Fezao187 <katlegomaredi9@gmail.com>
2024-08-30 12:57:44 +0200

    Exercise 7.3

---
+++ book1/lesson_07/exercise3/src/exercise7_3/ShoppingCart.java
@@ -0,0 +1,18 @@
+
+package exercise7_3;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        int int1;
+        
+	//  Declare and initialize variables of type long, float, and char.
+        long long1= 20000000000L;
+        float float1= 2.5F;
+        char char1= 'A';
+        // Print the long variable.
+    System.out.println("Long value: "+long1);
+	// Assign the long to the int and print the int variable.
+        int1 =(int) long1;
+        System.out.println("Int value from long: "+int1);
+    }
+}
