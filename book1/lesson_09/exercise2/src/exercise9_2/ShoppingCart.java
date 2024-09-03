80b415b7c832aa138977b49570fdb595ac4e6f97
Fezao187 <katlegomaredi9@gmail.com>
2024-09-03 11:37:49 +0200

    Lesson 9 quiz

--- book1/lesson_09/exercise2/src/exercise9_2/ShoppingCart.java
+++ book1/lesson_09/exercise2/src/exercise9_2/ShoppingCart.java
@@ -3,13 +3,13 @@ package exercise9_2;
 
 public class ShoppingCart {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // Declare, instantiate, and initialize a Customer object
         Customer cust1 = new Customer("Jane", "4445644343534");
 
 
-	// Print the customer object name
-    System.out.println(cust1.getName());
-        
+        // Print the customer object name
+        System.out.println(cust1.getName());
+
     }
 }
