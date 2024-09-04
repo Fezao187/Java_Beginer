f9b934a06ff90102c76d988b5502cd7842a33b3c
Fezao187 <katlegomaredi9@gmail.com>
2024-09-04 18:05:56 +0200

    Practice 9.1

---
+++ book1/lesson_09/practice1/src/soccer/Player.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package soccer;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Player {
+    
+    private String playerName;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
