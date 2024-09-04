b61700b3c6734551297e70f551753840af68d974
Fezao187 <katlegomaredi9@gmail.com>
2024-09-04 21:22:13 +0200

    Exercise 10.3

---
+++ book1/lesson_10/exercise3/src/exercise10_3/ShoppingCart.java
@@ -0,0 +1,11 @@
+
+package exercise10_3;
+
+import exercise10_3.Order;
+
+public class ShoppingCart {
+    public static void main(String args[]){
+        Order order = new Order("Rick Wilson", 910.00, "VA", Order.NONPROFIT);
+        System.out.println("Discount is: "+ order.getDiscount());
+    }
+}
