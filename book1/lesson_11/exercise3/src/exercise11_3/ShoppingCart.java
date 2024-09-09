b0aa4d82d09c06c68fd394cc3a9e54148f2006f1
Fezao187 <katlegomaredi9@gmail.com>
2024-09-09 12:18:24 +0200

    Practice 11.3

---
+++ book1/lesson_11/exercise3/src/exercise11_3/ShoppingCart.java
@@ -0,0 +1,30 @@
+
+package exercise11_3;
+
+import exercise11_3.Item;
+
+public class ShoppingCart {
+    Item[] items = {new Item("Shirt",25.60),
+                    new Item("WristBand",0), 
+                    new Item("Pants",35.99)};
+    
+    public static void main(String[] args){
+        ShoppingCart cart = new ShoppingCart();
+        cart.displayTotal();
+    }
+
+    // Use a standard for loop to iterate through the items array, adding up the total price
+    //    Skip any items that are back ordered.  Display the Shopping Cart total.
+    public void displayTotal(){
+        double total = 0.0;
+        for(int i = 0; i < items.length; i++){
+            System.out.println("Item: "+ items[i].getDesc() + "\nPrice: " + items[i].getPrice() +"\n");
+            if(items[i].isBackOrdered()){
+                continue;
+            }else {
+                total += items[i].getPrice();
+            }
+        }
+        System.out.println("Total: " + total);
+    }
+}
