5debfa37904bd51fc4a707ec9b846d58dbb25233
Fezao187 <katlegomaredi9@gmail.com>
2024-09-17 10:40:06 +0200

    Dummies Book 2 Chapter 3

---
+++ book2/chapter1-2/src/HelloApp.java
@@ -0,0 +1,9 @@
+//Listing 1.1 helloApp
+public class HelloApp {
+        public static void main(String[] args)
+        {
+            System.out.println("Hello, World!");
+        }
+}
+//javac HelloApp.java
+//java HelloApp
