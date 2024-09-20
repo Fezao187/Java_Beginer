953619b25e10bb2e337598c1dbd3919abce44c9a
Fezao187 <katlegomaredi9@gmail.com>
2024-09-20 02:13:16 +0200

    Dummies Book 4 Chapter 5

---
+++ book5/chapter5/src/listing5_1/GenStack.java
@@ -0,0 +1,28 @@
+package listing5_1;
+
+import java.util.*;
+
+//The GenStack Class
+public class GenStack<E> {
+    private final LinkedList<E> list = new LinkedList<E>();
+
+    public void push(E item) {
+        list.addFirst(item);
+    }
+
+    public E pop() {
+        return list.poll();
+    }
+
+    public E peek() {
+        return list.peek();
+    }
+
+    public boolean hasItems() {
+        return !list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
---
+++ book5/chapter5/src/listing5_2/GenQueue.java
@@ -0,0 +1,28 @@
+package listing5_2;
+
+import java.util.LinkedList;
+
+public class GenQueue<E> {
+    private final LinkedList<E> list = new LinkedList<E>();
+
+    public void enqueue(E item) {
+        list.addLast(item);
+    }
+
+    public E dequeue() {
+        return list.poll();
+    }
+
+    public boolean hasItems() {
+        return !list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public void addItems(GenQueue<? extends E> q) {
+        while (q.hasItems())
+            list.addLast(q.dequeue());
+    }
+}
